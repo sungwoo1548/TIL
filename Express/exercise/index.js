6443dbcc9c77c20acd4066ac5b06568458c23e9a
sungwoo1548 <swo1548@gmail.com>
2019-11-05 11:01:05 +0900

    set exercise

--- Express/exercise/index.js
+++ Express/exercise/index.js
@@ -2,8 +2,30 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const data = [
+    { id: 1, name: "sungwoo", email: "ksw@email.com", birthDay: "1988-05-05" },
+]
+
 app.get("/", (req, res) => {
+    console.log("hi");
     res.send("hello!")
+});
+app.get("/api/users", (req, res) => {
+    res.json(data);
+})
+
+app.post("/api/newuser", (req, res) => {
+
+    const newUser = {
+        id: data.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        birthDay: req.body.birthDay
+    }
+    data.push(newUser);
+    res.json(data);
 });
 
 const port = process.env.PORT || 3000;
