2a249e38d7e519d499da8d37080cea4624c17743
sungwoo1548 <swo1548@gmail.com>
2019-11-05 10:36:39 +0900

    done basic

---
+++ Express/README.md
@@ -0,0 +1,27 @@
+# sever...
+
+## API
+### `interpace` 
+
+​	:  사물 간 또는 사물과 인간 간의 의사소통이 가능하도록 일시적 혹은 영속적인 접근을 목적으로 만들어진 물리적, 가상적 매개체(접점)를 의미 
+
+
+
+## User입력 방지
+
+### `@hapi/joi`
+
+[doc]: https://hapi.dev/family/joi/?v=16.1.7
+
+​	yarn add @hapi/joi
+
+
+
+## 보안 모듈
+
+### `helmet`
+
+[doc]: https://helmetjs.github.io/docs/
+
+​	yarn add helmet
+
--- Express/basic/index.js
+++ Express/basic/index.js
@@ -1,30 +1,80 @@
 const express = require("express");
+const Joi = require("@hapi/joi");
+const helmet = require("helmet");
 const app = express();
+
+app.use(helmet()); // 상단에 선언해야함
+app.use(express.json());
 
 const courses = [
     { id: 1, name: "happy hacking!" },
     { id: 2, name: "real artist ship@" },
-    { id: 3, name: "see the invisible~" },
+    { id: 3, name: "see the invisible~", email: "hphk@email.com" },
 ];
+
+function getCourseById(id) {
+    const courseId = parseInt(id);
+    return courses.find(el => el.id === courseId);
+}
+
+function validate(input) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(10).alphanum().required(),
+        email: Joi.string().email(),
+    });
+
+    return schema.validate(input);
+}
 
 app.get("/", (req, res) => {
     res.send("해피해킹 입니다.");
 });
-
 app.get("/api/courses", (req, res) => {
     res.send(courses);
 });
-app.get("/api/courses/:id", (req, res) => {
-    console.log(req.params.id);
-    const id =parseInt(req.params.id);
-    const course = courses.find(el => el.id === id);
-    if(!course){
+app.get("/api/courses/:id", (req, res) => {    // .../api/courses/2 
+    const course = getCourseById(req.params.id);
+    if (!course) {
         res.status(404).send("해당 course가 없습니다.");
+    } else {
+        res.send(course);
     }
+});
+app.post("/api/course", (req, res) => {
+    const { value, error } = validate(req.body);
+    if (error) {
+        res.send(error.details[0].message);
+        return;
+    }
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name,
+        email: req.body.email
+    };
+    courses.push(course);
     res.send(course);
-});
-app.post("/api/course",(req,res)=>{
 
 });
+app.patch("/api/course", (req, res) => {  // put은 한가지 요소, patch 일부 수정
+    const course = getCourseById(req.params.id);
+    if (!course) {
+        res.send("해당하는 데이터가 없습니다");
+    } else {
+        const name = req.body.name;
+        course.name = name;
+        res.send(course);
+    }
+});
+app.delete("/api/course", (req, res) => {
+    const course = getCourseById(req.params.id);
+    if (!course) {
+        res.send("해당하는 데이터가 없습니다");
+    } else {
+        const index = courses.indexOf(course);
+        courses.splice(index, 1);
+        res.send(course);
+    }
+});
 
-app.listen(3000, () => console.log("Server Listen on port 3000....."));
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`Server Listen on port ${port}.....`));
