175399340b602b7f2c68e144e7ee894d1f261747
sungwoo1548 <swo1548@gmail.com>
2019-11-05 14:10:08 +0900

    done exercise

--- Express/exercise/index.js
+++ Express/exercise/index.js
@@ -3,47 +3,87 @@ const Joi = require("@hapi/joi");
 
 const app = express();
 
+const users = [
+  { id: 1, name: "binsan", email: "binsan@binsan.com", birthDay: "11-05-1999" }
+];
+let userId = 2;
+
 app.use(express.json());
 
-const data = [
-    { id: 1, name: "sungwoo", email: "ksw@email.com", birthDay: "1988-05-05" },
-]
-
-//Joi ; 입력값 검증 schema
-function validate(inputObject) {
-    const schema = Joi.object({
-        name: Joi.string().min(2).required(),
-        email: Joi.string().email(),
-        birthDay: Joi.date()
-    })
-    return schema.validate(inputObject)
+function getUser(id) {
+  return users.find(user => user.id === parseInt(id));
 }
+function validate(input) {
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(2)
+      .required(),
+    email: Joi.string()
+      .email()
+      .required(),
+    birthDay: Joi.date().less("1-1-2000")
+  });
+  return schema.validate(input);
+}
+//CRUD
 
-app.get("/", (req, res) => {
-    console.log("hi");
-    res.send("hello!")
-});
+//READ
 app.get("/api/users", (req, res) => {
-    res.json(data);
-})
+  res.send(users);
+});
+app.get("/api/users/:id", (req, res) => {
+  const user = getUser(req.params.id);
+  if (!user) {
+    res.send("유저가 없습니다ㅠㅠ");
+    return;
+  }
+  res.send(user);
+});
+//CREATE
+app.post("/api/user", (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    res.send(error.details[0].message);
+    return;
+  }
 
-app.post("/api/newuser", (req, res) => {
-    const { value, error } = validate(req.body);
-    if (error) {
-        res.send(error.details[0].message);
-        return;
-    }
-
-    const newUser = {
-        id: data.length + 1,
-        name: req.body.name,
-        email: req.body.email,
-        birthDay: req.body.birthDay
-    }
-    data.push(newUser);
-    res.json(data);
+  const user = {
+    id: userId,
+    name: req.body.name,
+    email: req.body.email,
+    birthDay: req.body.birthDay
+  };
+  userId++;
+  users.push(user);
+  res.send(user);
+});
+//UPDATE
+app.patch("/api/user/:id", (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    res.send(error.details[0].message);
+    return;
+  }
+  const user = getUser(req.params.id);
+  const { name, email, birthDay } = req.body;
+  user.name = name;
+  user.email = email;
+  user.birthDay = birthDay;
+  res.send(user);
+});
+//DELETE
+app.delete("/api/user/:id", (req, res) => {
+  const user = getUser(req.params.id);
+  if (!user) {
+    res.send("해당하는 데이터가 없습니다");
+  } else {
+    const index = users.indexOf(users);
+    users.splice(index, 1);
+    res.send(user);
+  }
 });
 
 const port = process.env.PORT || 3000;
-app.listen(3000, () => console.log(`Server Listen on port ${port}.....`));
-
+app.listen(port, () => {
+  console.log("서버를 돌리고 있습니다...");
+});
