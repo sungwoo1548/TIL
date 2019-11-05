fcb74a80b5478d9bc19015f29920c45f4f1f7214
sungwoo1548 <swo1548@gmail.com>
2019-11-05 11:28:45 +0900

    set validate function

--- Express/exercise/index.js
+++ Express/exercise/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Joi = require("@hapi/joi");
 
 const app = express();
 
@@ -7,6 +8,16 @@ app.use(express.json());
 const data = [
     { id: 1, name: "sungwoo", email: "ksw@email.com", birthDay: "1988-05-05" },
 ]
+
+//Joi ; 입력값 검증 schema
+function validate(inputObject) {
+    const schema = Joi.object({
+        name: Joi.string().min(2).required(),
+        email: Joi.string().email(),
+        birthDay: Joi.date()
+    })
+    return schema.validate(inputObject)
+}
 
 app.get("/", (req, res) => {
     console.log("hi");
@@ -17,6 +28,11 @@ app.get("/api/users", (req, res) => {
 })
 
 app.post("/api/newuser", (req, res) => {
+    const { value, error } = validate(req.body);
+    if (error) {
+        res.send(error.details[0].message);
+        return;
+    }
 
     const newUser = {
         id: data.length + 1,
