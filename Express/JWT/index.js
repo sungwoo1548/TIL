1381dcca4b5784ece1dfc108e7e9d9fbf2ec8784
sungwoo1548 <swo1548@gmail.com>
2019-11-11 11:33:08 +0900

    done jwt token example

--- Express/JWT/index.js
+++ Express/JWT/index.js
@@ -26,4 +26,13 @@ app.post("/login", (req, res, next) => {
         secretKey,
         { expiresIn: "1m" } // 1분,
     );
-})
+    res.json({ jwt: token });
+});
+
+app.get("/personal", (req, res, next) => { // header에 Authorization 에 토큰 정보 확인
+    const { data, error } = verifyToken(req);
+    if (data) res.json(data);
+    if (error) res.json(error);
+});
+
+app.listen(3000, () => console.log("서버가동중...."));
